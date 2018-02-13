9e2234b65b347ea1b6fdf313e3f9f77b2f60663d
ikhsan07 <ikhsanmiftah123@gmail.com>
2018-02-13 20:02:16 +0700

    Update Neh.js

--- Neh.js
+++ Neh.js
@@ -21,7 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*1)+ 1);
 var xxx=null;
-if(x=="1"){xxx="blog-page_13.html"}a_to_ve[i].href="http://download-oprekersphone-link.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="blog-page_13.html"}a_to_ve[i].href="https://download-oprekersphone-link.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
